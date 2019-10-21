24a20907dfae120552bd27a8094624ddcebb406c
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-21 11:39:41 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core')
+const fetch = require('node-fetch')
+
+try {
+  const makeRequest = async () => {
+    const url = core.getInput('url')
+    const selector = core.getInput('selector')
+    const test = core.getInput('test') || ''
+
+    const req = new URL(url)
+    req.searchParams.set('url', url)
+    req.searchParams.set('selector', selector)
+
+    const res = await fetch(req)
+    const json = await res.json()
+
+    console.log(test)
+    console.log(json)
+  }
+
+  makeRequest()
+} catch (error) {
+  core.setFailed(error.message)
+}
